a9e9c56cb9fa7b3dba93d7f055338db7996bfd3c
Shestakov Alexey <Shestakov.a.mail.work@gmail.com>
2020-12-05 17:41:35 +0200

    imported Component and scc to PopupWindow + PopupWindow class created

---
+++ src/components/popupWindow/popupWindow.js
@@ -0,0 +1,16 @@
+import  { Component } from "../component";
+import "./popupWindow.css";
+
+
+export class PopupWindow extends Component{
+    constructor(parentSelector) {
+        super(parentSelector);
+
+    }
+
+    render() {
+        super.render();
+
+    }
+
+}
