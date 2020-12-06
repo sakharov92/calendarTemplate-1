6d9950300b04787904988d0e50c4513e6a84d403
Sakharov Denys <sakharov92@gmail.com>
2020-12-06 21:20:42 +0200

    add updateteamRow function & refactoring

--- src/components/Calendar/teamCellComponent.js
+++ src/components/Calendar/teamCellComponent.js
@@ -8,31 +8,6 @@ export class TeamCell extends Component {
     this.depTeamInfo = depTeamInfo;
     this.component.className = "teamInfo";
   }
-
-  updateTeamCells(newDate) {
-    const daysInPreviousMonth = this.monthLength;
-    this.monthLength = new Date(
-      newDate.getFullYear(),
-      newDate.getMonth() + 1,
-      0,
-    ).getDate();
-    const daysList = Array.prototype.slice.call(
-      this.component.querySelectorAll(".dayCellOfMainRow"),
-    );
-    if (this.monthLength < daysInPreviousMonth) {
-      for (let index = this.monthLength; index < daysInPreviousMonth; index++) {
-        daysList[index].remove();
-      }
-    } else if (this.monthLength > daysInPreviousMonth) {
-      for (let index = daysInPreviousMonth; index < this.monthLength; index++) {
-        const dayCell = document.createElement("td");
-        dayCell.className = "dayCellOfMainRow";
-        daysList.push(dayCell);
-        daysList[index - 1].after(dayCell);
-      }
-    }
-  }
-
   render() {
     super.render();
   }
--- src/components/Calendar/teamRowComponent.js
+++ src/components/Calendar/teamRowComponent.js
@@ -6,15 +6,13 @@ import { TeamSum } from "./teamSumComponent";
 export class TeamRowComponent extends Component {
   constructor(parentSelector, depTeamInfo, monthLength, date) {
     super(parentSelector, "tr");
-    this.teamMembers = [];
     this.date = date;
     this.monthLength = monthLength;
     this.depTeamInfo = depTeamInfo;
     this.component.className = `mainRow ${this.depTeamInfo.name
       .split(" ")[0]
       .toLowerCase()}`;
-
-
+    this.daysContext = [];
   }
 
   generateTeamHeader() {
@@ -22,20 +20,43 @@ export class TeamRowComponent extends Component {
     const teamName = new TeamName(this.component, this.depTeamInfo, this.date);
     this.component.append(teamName.component);
 
-    for (let index = 0; index < this.monthLength; index++) {
+    for (let index = 0; index < 31; index++) {
       // insert TeamCell
       const teamCell = new TeamCell(this.component,
         this.depTeamInfo,
         this.monthLength,
         this.date,
       );
+      this.daysContext.push(teamCell);
       this.component.append(teamCell.component);
+      if (index >= this.monthLength) {
+        teamCell.hide();
+      }
     }
     // insert TeamSum
     const teamSum = new TeamSum();
     this.component.append(teamSum.component);
   }
-  
+
+  updateTeamHeader(newDate) {
+    
+    const daysInPreviousMonth = this.monthLength;
+    this.monthLength = new Date(
+      newDate.getFullYear(),
+      newDate.getMonth() + 1,
+      0,
+    ).getDate();
+   
+    if (this.monthLength < daysInPreviousMonth) {
+      for (let index = this.monthLength; index < daysInPreviousMonth; index++) {
+        this.daysContext[index].hide();
+      }
+    } else if (this.monthLength > daysInPreviousMonth) {
+      for (let index = daysInPreviousMonth; index < this.monthLength; index++) {
+        this.daysContext[index].show();
+      }
+    }
+  }
 
   // rendering TeamRow
   render() {
