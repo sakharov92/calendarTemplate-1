61063e0be9f3ea64996857412eb6ebdf9569b607
Miroslav Strukov <miroslavstrukov3@gmail.com>
2020-12-06 17:20:48 +0200

    team wrapper

---
+++ src/components/Calendar/team.js
@@ -0,0 +1,35 @@
+import { Component } from "..";
+import { TeamHeader } from "./teamHeader";
+import { TeamItem } from "./teamItem";
+
+
+export class Team extends Component {
+    constructor(parentSelector, teamData, daysInCurrentMonth, date) {
+        super(parentSelector, "tbody")
+        this.teamData = teamData;
+        this.date = date;
+        this.daysInCurrentMonth = daysInCurrentMonth;
+        this.arrayTeamItemsContext = [];
+    }
+
+    generateTeam() {
+        this.teamHeaderContext = new TeamHeader(this.component, this.teamData, this.daysInCurrentMonth, this.date);
+        this.teamHeaderContext.render();
+        let members = this.teamData.members;
+        let teamName = this.teamData.name.split(" ")[0].toLowerCase();
+        for (let member of members){
+            let item = new TeamItem(this.component, member, teamName, this.daysInCurrentMonth, this.date);
+            this.arrayTeamItemsContext.push(item);
+            item.render();
+        }
+    }
+
+    updateTeam(newDate) {
+        this.teamHeaderContext.updateTeamHeader(newDate);
+        this.arrayTeamItemsContext.forEach(e => e.updateTeamItem(newDate))
+    }
+    render() {
+        this.generateTeam()
+        super.render();
+    }
+}
