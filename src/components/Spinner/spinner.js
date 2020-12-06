6bf097665e3a132d7f3224fdebb916bcb80aff66
Vitalii Butovych <v.butovych@gmail.com>
2020-12-06 19:43:54 +0200

    created-basic-spinner-structure

---
+++ src/components/Spinner/spinner.js
@@ -0,0 +1,15 @@
+import { PopupWindow } from "../PopupWindow";
+import "./spinner";
+
+
+export class Spinner extends PopupWindow {
+    constructor(parentSelector){
+        super(parentSelector)
+    }
+
+
+
+    render(){
+        super.render()
+    }
+}
